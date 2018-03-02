d388ca25424d72bed9037d0abf9938f1b84aaa4a
duboffik <36671836+duboffik@users.noreply.github.com>
2018-03-02 16:08:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+//решаем по формуле Лежандра
+//находим number%(5^1) + number%(5^2) + ... пока слагаемые больше или равны единице
+    var b =(Math.log(number)/Math.log(5));
+    var nuli = 0;
+    for (i=1; i <=b; i++) {
+        step = Math.pow(5,i);
+        nuli = nuli + (number-number%step)/step;
+    }
+    return nuli;
 }
